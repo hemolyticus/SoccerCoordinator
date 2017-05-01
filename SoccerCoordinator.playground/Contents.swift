57a951da67f5a871e32760406d0bd745454d9356
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-05-01 17:55:25 +1200

    Created individual dictionary of players

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,5 +1,94 @@
-//: Playground - noun: a place where people can play
+//Dictionary of players
 
-import UIKit
+let player1: [String: Any] = ["playerName": "Joe Smith",
+                              "heightInInches": 42,
+                              "experiencedInSoccer": true,
+                              "guardians": "Jim and Jan Smith" ]
 
-var str = "Hello, playground"
+
+let player2: [String: Any] = ["playerName": "Jill Tanner",
+                              "heightInInches": 36,
+                              "experiencedInSoccer": true,
+                              "guardians": "Clara Tanner" ]
+
+
+let player3: [String: Any] = ["playerName": "Bill Bon",
+                              "heightInInches": 43,
+                              "experiencedInSoccer": true,
+                              "guardians": "Sara and Jenny Bon" ]
+
+let player4  : [String: Any] = ["playerName": "Eva Gordon",
+                                "heightInInches": 45,
+                                "experiencedInSoccer": false,
+                                "guardians": "Wendy and Mike Gordon" ]
+
+let player5: [String: Any] = ["playerName": "Matt Gill",
+                              "heightInInches": 40,
+                              "experiencedInSoccer": false,
+                              "guardians": "Charles and Sylvia Gill" ]
+
+let player6: [String: Any] = ["playerName": "Kimmy Stein",
+                              "heightInInches": 41,
+                              "experiencedInSoccer": false,
+                              "guardians": "Bill and Hillary Stein" ]
+
+let player7: [String: Any] = ["playerName": "Sammy Adams",
+                              "heightInInches": 45,
+                              "experiencedInSoccer": false,
+                              "guardians": "Jeff Adams" ]
+
+let player8: [String: Any] = ["playerName": "Karl Saygan",
+                              "heightInInches": 42,
+                              "experiencedInSoccer": true,
+                              "guardians": "Heather Bledsoe" ]
+
+let player9: [String: Any] = ["playerName": "Suzane Greenberg",
+                              "heightInInches": 44,
+                              "experiencedInSoccer": true,
+                              "guardians": "Henrietta  Dumas" ]
+
+let player10: [String: Any] = ["playerName": "Sal Dali",
+                               "heightInInches": 41,
+                               "experiencedInSoccer": false,
+                               "guardians": "Gala Dali" ]
+
+let player11: [String: Any] = ["playerName": "Joe Kavalier",
+                               "heightInInches": 39,
+                               "experiencedInSoccer": false,
+                               "guardians": "Sam and Elaine Kavalier" ]
+
+let player12: [String: Any] = ["playerName": "Ben Finkelstein",
+                               "heightInInches": 44,
+                               "experiencedInSoccer": false,
+                               "guardians": "Aaron and Jill Finkelstein" ]
+
+let player13: [String: Any] = ["playerName": "Diego Soto",
+                               "heightInInches": 41,
+                               "experiencedInSoccer": true,
+                               "guardians": "Robin and Sarika Soto" ]
+
+let player14: [String: Any] = ["playerName": "Chloe Alaska",
+                               "heightInInches": 47,
+                               "experiencedInSoccer": false,
+                               "guardians": "David and Jamie Alaska" ]
+
+let player15: [String: Any] = ["playerName": "Arnold Willis",
+                               "heightInInches": 43,
+                               "experiencedInSoccer": false,
+                               "guardians": "Claire Willis" ]
+
+let player16: [String: Any] = ["playerName": "Phillip Helm",
+                               "heightInInches": 44,
+                               "experiencedInSoccer": true,
+                               "guardians": "Thomas Helm and Eva Jones" ]
+
+let player17: [String: Any] = ["playerName": "Les Clay",
+                               "heightInInches": 42,
+                               "experiencedInSoccer": true,
+                               "guardians": "Wynona Brown" ]
+
+let player18: [String: Any] = ["playerName": "Herschel Krustofski",
+                               "heightInInches": 45,
+                               "experiencedInSoccer": true,
+                               "guardians": "Hyman and Rachel Krustrofski" ]
+
